1a4ed0b384cf1b7aa2081c032c3a82f27a0dc7b0
Muhammad Taha <tahasiddiqi89@gmail.com>
2021-02-25 23:59:50 +0500

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCPblLT8OsI30xpDmj6VAfsopIeI5BnGRo",
+    authDomain: "whatsup-965d2.firebaseapp.com",
+    projectId: "whatsup-965d2",
+    storageBucket: "whatsup-965d2.appspot.com",
+    messagingSenderId: "821876384398",
+    appId: "1:821876384398:web:67a8730a49dfb38b025fdf"
+  };
