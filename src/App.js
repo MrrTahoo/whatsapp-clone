6ead0a4849428bfd582592380b50d8376693e939
Muhammad Taha <tahasiddiqi89@gmail.com>
2021-02-26 16:59:50 +0500

    Building UI

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from "react";
 import './App.css';
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
-
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <div className="app__body">
+        <Sidebar />
+        <Chat />
+      </div>
 
-
-      <Sidebar />
-      <Chat />
 
     </div>
   );
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge"
+import MoreVertIcon from "@material-ui/icons/MoreVert"
+import ChatIcon from "@material-ui/icons/Chat"
+import {Avatar,IconButton} from '@material-ui/core';
+
 
 function sidebar() {
     return <div className="sidebar">
-            <h1>i am a side bar</h1>
-        </div>;
-    
+        <div className="sidebar__header">
+            <Avatar src="https://i.pinimg.com/564x/a1/20/94/a12094c4d2027734d37610e3f5399363.jpg" />
+            <div className="sidebar__headerRight">
+                <IconButton>
+                    <DonutLargeIcon />
+                </IconButton>
+                <IconButton>
+                    <ChatIcon />
+                </IconButton>
+                <IconButton>
+                    <MoreVertIcon />
+                </IconButton>
+                
+            </div>
+        </div>
+    </div>;
+
 }
 
 export default sidebar
