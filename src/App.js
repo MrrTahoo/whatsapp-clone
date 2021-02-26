0dc040609d71267afc0d78598ff06a007abb036c
Muhammad Taha <tahasiddiqi89@gmail.com>
2021-02-26 15:29:08 +0500

    building file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import './App.css';
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>lets build a whatsApp clone</h1>
+
+
+      <Sidebar />
+      <Chat />
+
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Sidebar.css";
+
+function sidebar() {
+    return <div className="sidebar">
+            <h1>i am a side bar</h1>
+        </div>;
+    
+}
+
+export default sidebar
